b37d20f8ed383ed8ae222c83f314887421093f86
jaumet <jaume@nualart.cat>
2014-11-25 18:12:31 +1100

    reordering all

--- ARCHIVE/area-drupal/js/area.js
+++
@@ -1,3 +0,0 @@
-function area_pre(preview_node) {
-	$("#area_preview").html(preview_node);
-}
--- area-js-IR/data/csv2json-p2pv.py
+++
@@ -1,3 +0,0 @@
-
-
-"Name","Description","Homepage","Social networks accounts","E-mail","Contact page","Organisation Type","Physical address","Legal entity","Year of foundation","Language(s) of the platform","Phone","Keywords","Type of activity","The most important type of collaboration","Type of common resource resulting","User generated content license","Software platform license","Owner","Infrastructure provider","Other information","Scope"
